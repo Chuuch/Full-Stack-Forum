0e3f6272bb77d7bf33f9b5a9258423e629f7203f
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-21 12:44:07 +0200

    client side refactor

--- client/src/components/UserAvatar.tsx
+++
@@ -1,9 +0,0 @@
-import { FaUserCircle } from "react-icons/fa";
-
-export const UserAvatar = () => {
-  return (
-    <div>
-      <FaUserCircle size={32} className="cursor-pointer" />
-    </div>
-  );
-};
--- client/src/pages/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-  return <div>Oops! Nothing to see here..</div>;
-};
-
-export default NotFound;
