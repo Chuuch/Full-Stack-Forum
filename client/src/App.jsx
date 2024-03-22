042634fdaed810a4a03038159710d4515e68b77c
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 14:41:02 +0200

    Added layout routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import News from "./components/News.jsx";
+import Forum from "./pages/Forum.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Admin from "./pages/Admin.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route />
+      </Routes>
+      <Outlet />
+    </Router>
+  );
 };
 
 export default App;
