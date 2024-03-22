550d128d487ebeadcb9f6c79a580e610579f2daf
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 14:42:35 +0200

    Notifications base component

---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,13 @@
+import { IoNotifications } from "react-icons/io5";
+
+const Notifications = () => {
+  return (
+    <div>
+      <div className="flex place-self-center cursor-pointer hover:bg-gray-800 opacity-75 rounded-full self-center w-10 h-10 z-1">
+        <IoNotifications size={22} className="cursor-pointer m-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
