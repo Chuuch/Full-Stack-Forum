9374bd215ab5cbbe5d902f000dd742e9dfdb64f3
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 23:33:07 +0200

    fixed login path for navigation

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,9 +1,9 @@
-import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
-	const { currentUser } = useSelector((state) => state.user);
-	return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
