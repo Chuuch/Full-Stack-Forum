b5213808c05a2f2180317ea8bedcab11b6614d5c
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 23:00:05 +0200

    Modal toggle functionality in Forum page

--- client/src/pages/Forum.jsx
+++ client/src/pages/Forum.jsx
@@ -1,19 +1,39 @@
-import SinglePost from "../components/SinglePost.jsx";
+import { useState } from "react";
 import { IoAddCircleOutline } from "react-icons/io5";
 import { MdOutlineFilterAlt } from "react-icons/md";
 import { LiaSortSolid } from "react-icons/lia";
-import Modal from "../components/Modal.jsx";
-import { useState } from "react";
+import SinglePost from "../components/SinglePost.jsx";
+import CreatePostModal from "../components/CreatePostModal.jsx";
+import FilterPostsModal from "../components/FilterPostsModal.jsx";
+import SortPostsModal from "../components/SortPostsModal.jsx";
 
 const Forum = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPostModalOpen, setPostModalOpen] = useState(false);
+  const [isFilterModalOpen, setFilterModalOpen] = useState(false);
+  const [isSortModalOpen, setSortModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
+  const openPostModal = () => {
+    setPostModalOpen(true);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const closePostModal = () => {
+    setPostModalOpen(false);
+  };
+
+  const openFilterModal = () => {
+    setFilterModalOpen(true);
+  };
+
+  const closeFilterModal = () => {
+    setFilterModalOpen(false);
+  };
+
+  const openSortModal = () => {
+    setSortModalOpen(true);
+  };
+
+  const closeSortModal = () => {
+    setSortModalOpen(false);
   };
 
   return (
@@ -23,7 +43,7 @@ const Forum = () => {
       </h1>
       <div className="flex flex-row items-center justify-center text-gray-400 gap-x-4">
         <button
-          onClick={openModal}
+          onClick={openPostModal}
           className="flex flex-row items-center justify-center
         gap-x-2 mt-20 border border-gray-400 hover:border-gray-300 hover:text-gray-300 transition-all p-2 rounded-lg"
         >
@@ -31,6 +51,7 @@ const Forum = () => {
           Add Post
         </button>
         <button
+          onClick={openFilterModal}
           className="flex flex-row items-center justify-center
        gap-x-2 mt-20 border border-gray-400 p-2 rounded-lg
        hover:border-gray-300 hover:text-gray-300 transition-all"
@@ -39,6 +60,7 @@ const Forum = () => {
           Filter Posts
         </button>
         <button
+          onClick={openSortModal}
           className="flex flex-row items-center justify-center
        gap-x-2 mt-20 border border-gray-400 p-2 rounded-lg
        hover:border-gray-300 hover:text-gray-300 transition-all"
@@ -48,7 +70,12 @@ const Forum = () => {
         </button>
       </div>
       <div>
-        <Modal isOpen={isModalOpen} onClose={closeModal}/>
+        <CreatePostModal isOpen={isPostModalOpen} onClose={closePostModal} />
+        <FilterPostsModal
+          isOpen={isFilterModalOpen}
+          onClose={closeFilterModal}
+        />
+        <SortPostsModal isOpen={isSortModalOpen} onClose={closeSortModal} />
       </div>
       <div className="mt-10">
         <SinglePost />
