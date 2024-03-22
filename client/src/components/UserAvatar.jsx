538edc6eaf761676ac0e9fefdbc273f709e69748
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 23:33:35 +0200

    removed unnecessary part from component

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -1,20 +1,7 @@
 import UserDropdown from "./UserDropdown";
 
 const UserAvatar = () => {
-  // const [avatarURL, setAvatarURL] = useState('');
 
-  // useEffect(() => {
-  // 	const storage = getStorage();
-  // 	const storageRef = ref(storage, `images/${uid}`);
-
-  // 	getDownloadURL(storageRef)
-  // 		.then((url) => {
-  // 			setAvatarURL(url);
-  // 		})
-  // 		.catch((error) => {
-  // 			console.error('Error getting download URL:', error);
-  // 		});
-  // }, [uid]);
 
   return (
     <div>
