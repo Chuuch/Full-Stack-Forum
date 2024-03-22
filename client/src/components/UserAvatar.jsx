7ef7886727ce61b5247d8ac1ffe7547813b63cab
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-22 22:59:24 +0200

    refactored user avatar component

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -1,4 +1,4 @@
-import { FaUserCircle } from "react-icons/fa";
+import UserDropdown from "./UserDropdown";
 
 const UserAvatar = () => {
   // const [avatarURL, setAvatarURL] = useState('');
@@ -17,8 +17,8 @@ const UserAvatar = () => {
   // }, [uid]);
 
   return (
-    <div className="flex place-self-center my-auto cursor-pointer hover:bg-gray-800 opacity-75 rounded-full self-center w-10 h-10 z-1">
-      <FaUserCircle className="cursor-pointer fill-gray-300 m-auto" size={32} />
+    <div>
+      <UserDropdown />
     </div>
   );
 };
