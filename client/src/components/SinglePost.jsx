ab5bef6d71cbef60686c2d4e47ae6c7bf19e1447
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-26 14:13:37 +0200

    Single post refactored

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -1,26 +1,34 @@
 /* eslint-disable react/prop-types */
-import UserAvatar from "../components/UserAvatar.jsx";
 import { AiOutlineLike } from "react-icons/ai";
 import { GoComment } from "react-icons/go";
 import { GoInfo } from "react-icons/go";
-import PostDropdown from "./PostDropdown.jsx";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import EditPostModal from "./EditPostModal.jsx";
+import DeletePostModal from "./DeletePostModal.jsx";
+import { useState } from "react";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 const SinglePost = ({ post }) => {
-  const [author, setAuthor] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
-  useEffect(() => {
-    const fetchAuthor = async () => {
-      try {
-        const response = await axios.get(`/api/user/${post.userRef}`);
-        setAuthor(response.data);
-      } catch (error) {
-        console.error("Error fetching author:", error);
-      }
-    };
-    fetchAuthor();
-  }, [post.userRef]);
+  const openEditModal = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
 
   return (
     <article className="flex flex-row bg-gray-800 rounded-lg w-[540px] h-[240px] shadow-md p-4">
@@ -29,21 +37,45 @@ const SinglePost = ({ post }) => {
           <strong className="text-lg text-gray-400">{post.title}</strong>
           <p>{post.content}</p>
         </div>
-        <div className="flex flex-col mt-6">
-          <UserAvatar />
+        <div className="flex flex-col mt-14">
+          <FaUserCircle size={32} className="text-gray-400" />
         </div>
-        <p className="text-xs text-gray-500">posted by: {author.username}</p>
-        <div className="flex flex-row items-center justify-center space-x-96">
+        <p className="text-xs text-gray-500">
+          posted by: {post.userRef.username}
+        </p>
+        <div className="flex flex-row items-center justify-center space-x-[360px] mt-3">
           <div className="flex flex-row items-center justify-center gap-x-2">
             <AiOutlineLike size={25} className="cursor-pointer" />
             <GoComment size={25} className="cursor-pointer" />
-            <GoInfo size={25} className="cursor-pointer" />
+            <Link to={`/forum/${post._id}`}>
+              <GoInfo size={25} />
+            </Link>
           </div>
-          <div>
-            <PostDropdown />
+          <div className="flex gap-x-2">
+            <CiEdit
+              onClick={openEditModal}
+              size={28}
+              className="text-gray-200 cursor-pointer"
+            />
+
+            <MdDeleteOutline
+              onClick={openDeleteModal}
+              size={28}
+              className="cursor-pointer"
+            />
           </div>
         </div>
       </div>
+      <EditPostModal
+        isOpen={isEditModalOpen}
+        onClose={closeEditModal}
+        post={post}
+      />
+      <DeletePostModal
+        postId={post._id}
+        isOpen={isDeleteModalOpen}
+        onClose={closeDeleteModal}
+      />
     </article>
   );
 };
