d1e4afc46ba106c55be34978a9befaef53daafbf
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-26 14:14:17 +0200

    Home page component with type animation & stats

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Stats from "../components/Stats";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-x-10">
-      <div className="absolute top-[350px] left-[220px] flex flex-col items-start  text-8xl">
+      <div className="absolute top-[350px] lg:left-[220px] flex flex-col items-start md:left-[70px] md:text-6xl lg:text-8xl text-4xl">
         <TypeAnimation
           sequence={[
             "Welcome to DevTalk!",
@@ -22,14 +22,16 @@ const Home = () => {
           repeat={Infinity}
           cursor="text-sky-500"
         />
-        <p className="text-3xl pl-2 mt-4 text-gray-500">
+        <p className="text-3xl md:w-full pl-2 mt-4 text-gray-500">
           Made by developers, for developers. Here to learn, grow and help.
         </p>
       </div>
-      <div className="flex flex-row items-center absolute left-[230px] top-[500px]">
-        <PiCode size={240} className="text-sky-500 animate-pulse" />
+      <div className="flex flex-row items-center absolute lg:left-[240px] md:left-[80px] left-[230px] top-[500px]">
+        <PiCode className="text-sky-500 animate-pulse md:w-40 md:h-40" />
       </div>
-      <Stats />
+      <div className="flex lg:left-[450px] md:left-[260px] md:top-[525px] absolute top-[560px] left-[520px]">
+        <Stats />
+      </div>
     </div>
   );
 };
